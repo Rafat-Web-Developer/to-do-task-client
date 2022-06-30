83a58c0479043055588511072bb0b508363c408f
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-06-30 15:21:04 +0600

    Design Navbar page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import Navbar from "./Pages/shared/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
       </Routes>
--- src/Pages/shared/Navbar.js
+++ src/Pages/shared/Navbar.js
@@ -1,10 +1,52 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <header>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            TO-DO-TASK
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/completedTask">
+                  Completed Task
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/toDoList">
+                  To-do-List
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="btn btn-primary" to="/login">
+                  Login
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
 
